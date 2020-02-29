758f1558eaa0c73a84cfeaab62c221ac50a48ee3
bbecomp99 <43157160+bbecomp99@users.noreply.github.com>
2020-02-29 13:35:05 -0500

    Delete provider.tf

--- dr-vmware-k8s/terraform/vkube/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "vsphere" {
-  user           = "terraform@vsphere.local"
-  password       = "qoCAahsgdbc76sj2#"
-  vsphere_server = "vcenter01.kaplaninc.com"
-  allow_unverified_ssl = true
-}
